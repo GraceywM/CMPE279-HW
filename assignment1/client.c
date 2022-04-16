d53c64a8165e8cfc5cdc1612ce463b568195204a
GraceywM <48034030+GraceywM@users.noreply.github.com>
2022-04-15 23:06:20 -0700

    update

--- assignment1/client.c
+++ assignment1/client.c
@@ -1,18 +1,21 @@
-// Client side C/C++ program to demonstrate Socket programming 
+// Client side C/C++ program to demonstrate Socket programming
 #include <stdio.h> 
 #include <sys/socket.h> 
 #include <stdlib.h> 
 #include <netinet/in.h> 
 #include <string.h> 
+#include <arpa/inet.h>
+#include <unistd.h>
+
 #define PORT 8080 
    
 int main(int argc, char const *argv[]) 
 { 
-    struct sockaddr_in address; 
     int sock = 0, valread; 
     struct sockaddr_in serv_addr; 
     char *hello = "Hello from client"; 
     char buffer[1024] = {0}; 
+
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
     { 
         printf("\n Socket creation error \n"); 
@@ -31,14 +34,16 @@ int main(int argc, char const *argv[])
         return -1; 
     } 
    
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     { 
-        printf("\nConnection Failed \n"); 
+        printf("\nConnection Failed\n"); 
         return -1; 
-    } 
-    send(sock , hello , strlen(hello) , 0 ); 
+    }
+
+    send(sock, hello, strlen(hello), 0 ); 
     printf("Hello message sent\n"); 
-    valread = read( sock , buffer, 1024); 
-    printf("%s\n",buffer ); 
+    valread = read(sock, buffer, 1024); 
+    printf("Read %d bytes: %s\n", valread, buffer);
+
     return 0; 
 } 
